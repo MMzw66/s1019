55bbadbf708d2d8fb1c80129e7dc69f8f449a90e
MMzw66 <924mzw924>
2016-11-01 14:43:39 +0800

    新闻列表功能

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -24,6 +24,42 @@ namespace WebApplication1.Controllers
             return View();
 
         }
+        /// <summary>
+        /// 欢迎页面 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Welcome()
+        {
+            var a = 100;
+            var sum = 0;
+            for (var i = 1; i <= a; i++)
+            {
+                sum += i;
+            }
+
+            ViewBag.sum = sum;
+            return View();
+        }
+
+        /// <summary>
+        /// 新闻列表
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult List()
+        {
+            string[] data = new string[] { 
+                "新闻1XXXXXXXXXXXXXXXXXXXXXXXX",
+                "新闻2XXXXXXXXXXXXXXXXXXXXXXXX",
+                "新闻3XXXXXXXXXXXXXXXXXXXXXXXX",
+                "新闻4XXXXXXXXXXXXXXXXXXXXXXXX",
+                "新闻5XXXXXXXXXXXXXXXXXXXXXXXX"
+               };
+
+            //ViewBag.data = data;
+            //ViewData["data"] = data;
+            ViewData.Model = data;
+            return View();
+        }
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
